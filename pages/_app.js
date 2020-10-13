03f77d0e7909ded7dbe2c62c33671cb4ed13d7db
Myha <miha@vidakovic.si>
2020-10-13 13:16:54 +0200

    added meta

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import '../styles/styles.scss'
 
 import Header from "../components/Header";
 
-function MyApp ({ Component, pageProps, router }) {
+function WeightApp ({ Component, pageProps, router }) {
 	return (
 		<>
 			<motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
@@ -16,6 +16,6 @@ function MyApp ({ Component, pageProps, router }) {
 	)
 }
 
-MyApp.getInitialProps = async (appContext) => ({ ...await App.getInitialProps(appContext) })
+WeightApp.getInitialProps = async (appContext) => ({ ...await App.getInitialProps(appContext) })
 
-export default MyApp
+export default WeightApp
--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,16 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Weight</title>
-				<link rel="icon" href="/favicon.ico" />
+				<title>Home - WeightApp</title>
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+				<meta name="apple-mobile-web-app-title" content="Weight" />
+				<meta name="application-name" content="Weight" />
+				<meta name="msapplication-TileColor" content="#da532c" />
+				<meta name="theme-color" content="#222222" />
 			</Head>
 
 			<main className="home">
