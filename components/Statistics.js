27c7de349ec6a3c769f09a44d3a2e8eced9d7adb
Myha <miha@vidakovic.si>
2020-10-31 20:11:09 +0100

    added statistic

---
+++ components/Statistics.js
@@ -0,0 +1,63 @@
+import React, {useEffect, useState} from 'react'
+import dayjs from "dayjs"
+require('dayjs/locale/sl')
+var isBetween = require('dayjs/plugin/isBetween')
+dayjs.extend(isBetween)
+
+export default function Statistics(props) {
+    const [allLostWeight, setAllLostWeight] = useState();
+    const [lostWeightWeek, setLostWeightWeek] = useState();
+
+    function getLostWeight() {
+        let newItems = [];
+        for (var i = props.data.length - 1; i >= 0; i--) {
+            if((dayjs(props.data[i].created_at).isBetween(dayjs().subtract(7, 'day'), dayjs()))) {
+                newItems.push(props.data[i])
+            }
+          }
+          setLostWeightWeek(newItems[0].weight - [...newItems].pop().weight)
+    }
+    function getAllLostWeight() {
+        setAllLostWeight([...props.data].pop().weight - props.data[0].weight)
+    }
+
+    useEffect(() => {
+        getLostWeight()
+        getAllLostWeight()
+    }, [props])
+
+    if(lostWeightWeek && allLostWeight) {
+        return (
+            <>
+                <h3>Statistics</h3>
+                <div className="boxes">
+                <div className="box">
+                        <span className="box__title">{allLostWeight.toFixed(1)} kg</span>
+                        <span className="box__subtitle">Weight lost over-all</span>
+                    </div>
+                    <div className="box">
+                        <span className="box__title">{lostWeightWeek.toFixed(1)} kg</span>
+                        <span className="box__subtitle">Weight lost last week</span>
+                    </div>
+                    <div className="box">
+                        <span className="box__title">{props.data.length}</span>
+                        <span className="box__subtitle">Days of logging weight</span>
+                    </div>
+                </div>
+            </>
+        )    
+    } else {
+        return (
+            <>
+                <h3>Statistics</h3>
+                <div className="boxes">
+                    <div className="box">
+                        <span className="box__title">0 kg</span>
+                        <span className="box__subtitle">Weight lost last week</span>
+                    </div>
+                </div>
+            </>
+        )    
+
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,95 @@
+import { useState, setState, useEffect } from "react";
 import Head from 'next/head'
 
 import Graph from '../components/Graph';
 import Add from '../components/Add';
+import Statistics from '../components/Statistics';
+import Loader from "react-loader-spinner";
 
 export default function Home() {
+	const [data, setData] = useState();
+
+	function fetchData() {
+		fetch("/api/weights")
+			.then(response => {
+				if (!response.ok) { throw response }
+				return response.json()  //we only get here if there is no error
+			})
+			.then(json => {
+				setData(json.data)
+			})
+			.catch(err => {
+				console.log(err)
+			});
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [])
 
 
-	return (
-		<>
-			<Head>
-				<title>Home - WeightApp</title>
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-				<link rel="manifest" href="/site.webmanifest" />
-				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-				<meta name="apple-mobile-web-app-title" content="Weight" />
-				<meta name="application-name" content="Weight" />
-				<meta name="msapplication-TileColor" content="#da532c" />
-				<meta name="theme-color" content="#222222" />
-			</Head>
+	if (data) {
+		return (
+			<>
+				<Head>
+					<title>Home - WeightApp</title>
+					<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+					<link rel="manifest" href="/site.webmanifest" />
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+					<meta name="apple-mobile-web-app-title" content="Weight" />
+					<meta name="application-name" content="Weight" />
+					<meta name="msapplication-TileColor" content="#da532c" />
+					<meta name="theme-color" content="#222222" />
+				</Head>
 
-			<main className="home">
-				<div className="container">
-					<div className="add">
-						<Add />
+				<main className="home">
+					<div className="container">
+						<div className="add">
+							<Add />
+						</div>
+						<div className="main">
+							<div className="statistics">
+								<Statistics data={data} />
+							</div>
+							<div className="graph">
+								<Graph data={data} />
+							</div>
+						</div>
 					</div>
-					<div className="graph">
-						<Graph />
+				</main>
+			</>
+		)
+	} else {
+		return (
+			<>
+				<Head>
+					<title>Home - WeightApp</title>
+					<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+					<link rel="manifest" href="/site.webmanifest" />
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+					<meta name="apple-mobile-web-app-title" content="Weight" />
+					<meta name="application-name" content="Weight" />
+					<meta name="msapplication-TileColor" content="#da532c" />
+					<meta name="theme-color" content="#222222" />
+				</Head>
+
+				<main className="home">
+					<div className="container">
+						<Loader
+							className="loading__spinner"
+							type="Rings"
+							color="#888"
+							height={100}
+							width={100}
+						/>
 					</div>
-				</div>
 			</main>
-		</>
-	)
+			</>
+
+		)
+	}
 }
