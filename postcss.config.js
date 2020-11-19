eaa9e1f578fc6bbf3ae9d854bf0abaa016b78b5a
Myha <miha@vidakovic.si>
2020-11-19 15:51:42 +0100

    ppa

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
+const purgecss = ["@fullhuman/postcss-purgecss", {
   // paths to all of the template files in the project
   content: ["./src/**/*.html", "./src/**/*.tsx", "./public/**/*.html"],
 
   // default extractor including tailwind's special characters
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-});
+}];
 
 module.exports = {
   plugins: [
