4b6e0f3aa3eb4b10ce4451fa2bab4dcd255cec1b
Myha <miha@vidakovic.si>
2020-11-19 15:45:10 +0100

    oo

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,20 @@
-const purgecss = [
-  '@fullhuman/postcss-purgecss',
-  {
-    // Specify the paths to all of the template files
-    content: [
-      './pages/**/*.{js,jsx,ts,tsx}',
-      './components/**/*.{js,jsx,ts,tsx}',
-    ],
-    // This is the function used to extract class names from the templates
-    defaultExtractor: (content) => {
-      // Capture as liberally as possible, including things like `h-(screen-1.5)`
-      const broadMatches = content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || [];
-      // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-      const innerMatches = content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || [];
-      return broadMatches.concat(innerMatches);
-    },
-  },
-];
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // paths to all of the template files in the project
+  content: ["./src/**/*.html", "./src/**/*.tsx", "./public/**/*.html"],
 
+  // default extractor including tailwind's special characters
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
 
 module.exports = {
   plugins: [
-    'tailwindcss',
-    'postcss-preset-env',
-  ],
-  }
+    require("tailwindcss"),
+    require("postcss-preset-env")({
+      stage: 1,
+      features: {
+        'focus-within-pseudo-class': false
+      }
+    }),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
+};
