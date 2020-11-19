40bd588f4ab750a502fe95d67ab9e295d3d0a703
Myha <miha@vidakovic.si>
2020-11-19 19:17:09 +0100

    postcs

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,6 @@
-const purgecss = ["@fullhuman/postcss-purgecss", {
-  // paths to all of the template files in the project
-  content: ["./src/**/*.html", "./src/**/*.tsx", "./public/**/*.html"],
-
-  // default extractor including tailwind's special characters
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-}];
-
 module.exports = {
-  plugins: [
-    "tailwindcss",
-    ["postcss-preset-env", {
-      stage: 1,
-      features: {
-        'focus-within-pseudo-class': false
-      }
-    }],
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
