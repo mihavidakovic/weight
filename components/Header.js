5c575fff38c0d8857daf16125792de8fe2c1301d
Myha <miha@vidakovic.si>
2020-12-17 09:54:41 +0100

    dev

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,13 @@
 import Link from 'next/link'
 import { useRouter } from "next/router";
-import {RiScales2Line} from "react-icons/ri"
+import { RiScales2Line } from "react-icons/ri"
 
 
 export default function Header() {
 	const router = useRouter();
 
 	return (
-		<header style={{padding: "1.125em 0", backgroundColor: "rgba(0,0,0,.8)", position: "fixed", width: "100%", left: 0, top: 0, zIndex: 999, backdropFilter: "blur(10px)", boxShadow: "0 2px 10px 0 rgba(0,0,0,.3)"}}>
+		<header style={{ padding: "1.125em 0", backgroundColor: "rgba(0,0,0,.8)", position: "fixed", width: "100%", left: 0, top: 0, zIndex: 999, backdropFilter: "blur(10px)", boxShadow: "0 2px 10px 0 rgba(0,0,0,.3)" }}>
 			<div className="container flex flex-row justify-between items-center">
 				<div className="logo cursor-pointer">
 					<Link href="/">
@@ -17,7 +17,7 @@ export default function Header() {
 						</div>
 					</Link>
 				</div>
-				<div className="nav flex flex-row">
+				<div className="nav flex flex-row items-center">
 					<Link href="/">
 						<span className={router.pathname == "/" ? "active text-white text-xs uppercase font-bold cursor-pointer transition mr-4 last:mr-0" : "text-gray-300 hover:text-gray-100 text-xs uppercase font-bold cursor-pointer transition mr-4 last:mr-0"}>
 							Home
@@ -28,6 +28,12 @@ export default function Header() {
 							Edit
 						</span>
 					</Link>
+					<span>
+						<select className="border-none outline-none">
+							<option value="kg">kg</option>
+							<option value="lb">lbs</option>
+						</select>
+					</span>
 				</div>
 			</div>
 		</header>
